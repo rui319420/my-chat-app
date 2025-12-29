9e627cdeb4f95b2e2b757a3d5de7e8209cfcab4b
Rui <s1F102501866@iniad.org>
2025-12-29 11:46:46 +0900

    feat: サイドバーの原型を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Header from "./header/header";
 import { Textarea } from "@/components/ui/textarea";
+import { Sidebar, SidebarProvider } from "@/components/ui/sidebar";
 
 
 export default function Home() {
@@ -12,6 +13,10 @@ export default function Home() {
     <div className="container">
       <Header></Header>
       <div className="contents">
+
+          <SidebarProvider>
+                  <Sidebar></Sidebar>
+          </SidebarProvider>
         <Textarea placeholder="ここに文字を入力できます。" id="message" />
         <div></div>
       </div>
