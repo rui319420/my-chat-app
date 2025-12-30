c22b6efb990337df9cc070edef9ade4efc146311
Rui <s1F102501866@iniad.org>
2025-12-30 10:45:50 +0900

    サイドバーの開閉ができるようになったがアイテムが表示されない

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,26 @@
 import { useState } from "react";
 import Header from "./header/header";
 import { Textarea } from "@/components/ui/textarea";
-import { Sidebar, SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 
-export default function Home() {
+
+export default function Home({ children }: { children: React.ReactNode }) {
   
 
   return (
     <div className="container">
       <Header></Header>
       <div className="contents">
-
-          <SidebarProvider>
-                  <Sidebar></Sidebar>
-          </SidebarProvider>
+        <SidebarProvider>
+          <AppSidebar />
+            <main>
+              <SidebarTrigger />
+              {children}
+            </main>
         <Textarea placeholder="ここに文字を入力できます。" id="message" />
-        <div></div>
+        </SidebarProvider>
       </div>
     </div>
   );
